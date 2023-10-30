88ce2d307bef731e9e28a9226bbf4479daaa202e
Abdul Taher Khan <49634197+abdGhost@users.noreply.github.com>
2023-10-30 12:59:25 +0530

    start working on chat_screen

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -1,5 +1,29 @@
+import 'dart:math';
+
+import 'package:emart_app/consts/consts.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
+  @override
+  void onInit() {
+    getUsername();
+    super.onInit();
+  }
+
   var currentNavIndex = 0.obs;
+
+  var userName = '';
+
+  getUsername() async {
+    var n = await firebaseFirestore
+        .collection(userCollection)
+        .where("id", isEqualTo: currentUser!.uid)
+        .get()
+        .then((value) {
+      if (value.docs.isNotEmpty) {
+        return value.docs.single['name'];
+      }
+    });
+    userName = n;
+  }
 }
