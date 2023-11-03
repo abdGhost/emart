ac762556608e005c1391f2469216e42685b0367b
abdul <ktaher642@gmail.com>
2023-11-03 14:44:33 +0530

    worked on message_screen for user

--- lib/views/messages_screen/messages_screen.dart
+++ lib/views/messages_screen/messages_screen.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:emart_app/consts/consts.dart';
 import 'package:emart_app/services/firestore_services.dart';
+import 'package:emart_app/views/chat_screens/chat_screen.dart';
+import 'package:get/get.dart';
 
 class MessagesScreen extends StatelessWidget {
   const MessagesScreen({super.key});
@@ -24,7 +26,42 @@ class MessagesScreen extends StatelessWidget {
               child: 'No Messages Yet!'.text.fontFamily(semibold).color(darkFontGrey).make(),
             );
           } else {
-            return Container();
+            var data = snapshot.data!.docs;
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                        itemCount: data.length,
+                        itemBuilder: (context, index) {
+                          return Card(
+                            child: ListTile(
+                              onTap: () {
+                                Get.to(
+                                  () => const ChatScreen(),
+                                  arguments: [
+                                    data[index]['friend_name'],
+                                    data[index]['to_id'],
+                                  ],
+                                );
+                              },
+                              leading: const CircleAvatar(
+                                backgroundColor: redColor,
+                                child: Icon(
+                                  Icons.person,
+                                  color: whiteColor,
+                                ),
+                              ),
+                              title: '${data[index]['friend_name']}'.text.fontFamily(semibold).color(darkFontGrey).make(),
+                              subtitle: '${data[index]['last_message']}'.text.make(),
+                            ),
+                          );
+                        }),
+                  ),
+                ],
+              ),
+            );
           }
         },
       ),
